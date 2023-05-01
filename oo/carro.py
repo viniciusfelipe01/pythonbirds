5a8ef92720e6e7e6d361f78ec0aa6d341cb0d2cd
viniciusfelipe01 <vinicius_ubisoft@hotmail.com>
2023-05-01 11:19:26 +0100

    Criado a classe Carro

--- oo/carro.py
+++ oo/carro.py
@@ -70,33 +70,57 @@ A direcao tera a responsabilidade de controlar a direcao. Ela oferece os seguint
         >>> direcao.valor
         'Norte'
 
-        #Exemplificando
+        #Testando motor
         >>> carro = Carro(direcao, motor)
         >>> carro.calcular_velocidade()
         0
-        >>> carro = acelerar()
+        >>> carro.acelerar()
         >>> carro.calcular_velocidade()
         1
-        >>> carro = acelerar()
+        >>> carro.acelerar()
         >>> carro.calcular_velocidade()
         2
-        >>> carro = frear()
+        >>> carro.frear()
         >>> carro.calcular_velocidade()
         0
         >>> carro.calcular_direcao()
-        >>> 'Norte'
+        'Norte'
         >>> carro.girar_a_direita()
         >>> carro.calcular_direcao()
-        >>> 'Leste'
+        'Leste'
         >>> carro.girar_a_esquerda()
         >>> carro.calcular_direcao()
-        >>> 'Norte'
+        'Norte'
         >>> carro.girar_a_esquerda()
         >>> carro.calcular_direcao()
-        >>> 'Oeste'
+        'Oeste'
 
 
 """
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
 
 NORTE = 'Norte'
 SUL = 'Sul'
