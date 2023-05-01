dbee2cab9f21b7427b4d2fb3b5acece04cd0eab1
viniciusfelipe01 <vinicius_ubisoft@hotmail.com>
2023-05-01 12:19:12 +0100

    Mais uma funcao para exemplificacao do Unitest

--- oo/teste_carro.py
+++ oo/teste_carro.py
@@ -7,3 +7,7 @@ class CarroTestCase(TestCase):
     def teste_velocidade_inicial(self):
         motor = Motor()
         self.assertEqual(0, motor.velocidade)
+    def teste_acelerar(self):
+        motor = Motor()
+        motor.acelerar()
+        self.assertEqual(1, motor.velocidade)
